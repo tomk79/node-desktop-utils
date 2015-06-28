c27258231527054c0391d4507afdc177b9932256
Tomoya Koyanagi <tomk79@gmail.com>
2015-06-29 01:50:58 +0900

    サポート対象をチェック

--- libs/main.js
+++ libs/main.js
@@ -2,6 +2,14 @@
  * desktop-utils
  */
 module.exports = new (function(){
+	var supported;
+	switch( process.platform ){
+		case 'darwin':
+		case 'win32':
+			supported = true; break;
+		default:
+			supported = false; break;
+	}
 
 	/**
 	 * アイテムを開く
@@ -10,9 +18,10 @@ module.exports = new (function(){
 	 * @return spawn
 	 */
 	this.open = function( item ){
+		if( !supported ){ return false; }
 		var spawn = require('child_process').spawn;
 		var cmd = 'open';
-		if(process.platform=='win32'){
+		if(process.platform == 'win32'){
 			cmd = 'explorer';
 			if( item.match(new RegExp('^(?:https?|data)\\:','i')) ){
 				// OS依存しないのでスルー
@@ -27,6 +36,8 @@ module.exports = new (function(){
 	 * ローカルデータディレクトリのパスを取得する
 	 */
 	this.getLocalDataDir = function( appName ){
+		if( !supported ){ return false; }
+		if( !(process.env.HOME||process.env.LOCALAPPDATA) ){ return false; }
 		var path = require('path');
 		if(typeof(appName)==typeof(0)||typeof(appName)==typeof(1.5)){appName = ''+appName;}
 		if(typeof(appName)!=typeof('')){return false;}
